a035ca3da32e604b7bdba214d8705f7e049b7115
srivaths.puranam <srivaths.puranam@sailotech.com>
2019-11-08 12:28:19 +0530

    spring

---
+++ HExample/src/example/H.java
@@ -0,0 +1,39 @@
+package example;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "srivaths2")
+public class H {
+	@Id
+	private int empNum;
+	private String empName;
+	private double empSalary;
+
+	public int getEmpNum() {
+		return empNum;
+	}
+
+	public void setEmpNum(int empNum) {
+		this.empNum = empNum;
+	}
+
+	public String getEmpName() {
+		return empName;
+	}
+
+	public void setEmpName(String empName) {
+		this.empName = empName;
+	}
+
+	public double getEmpSalary() {
+		return empSalary;
+	}
+
+	public void setEmpSalary(double empSalary) {
+		this.empSalary = empSalary;
+	}
+
+}
---
+++ HExample/src/example/Hib.java
@@ -0,0 +1,29 @@
+package example;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+public class Hib {
+
+	public static void main(String[] args) {
+		
+		Configuration cfg=new Configuration();
+		cfg.configure("H.cfg.xml");
+		SessionFactory factory = cfg.buildSessionFactory();
+		Session session = factory.openSession();
+		Transaction t = session.beginTransaction();
+		
+        H e = new H();
+        e.setEmpNum(100);
+    	 e.setEmpName("xyz");
+    	  e.setEmpSalary(100);
+    	  
+    	  session.persist(e);
+     	  t.commit();
+     	  session.close();
+	System.out.println("saved");	
+		
+	}
+}
